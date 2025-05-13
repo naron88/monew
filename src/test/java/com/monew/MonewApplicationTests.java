a0668ac3d89d647bcba3ec6ca53a13b1551faee5
narone <buzzin2426@gmail.com>
2025-05-13 11:48:18 +0900

    chore: 초기 작업

---
+++ src/test/java/com/monew/MonewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonewApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
